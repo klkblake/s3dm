a1f9183f24c8aafc4a863996d42f9b34c6229ddd
Kyle Blake <klkblake@gmail.com>
2012-07-10 17:57:13 +0930

    Add fixed-point Position.

---
+++ position.go
@@ -0,0 +1,13 @@
+package s3dm
+
+type Position struct {
+	X, Y, Z Fixed64
+}
+
+func (p Position) Add(v V3) Position {
+	return Position{p.X + NewFixed64(v.X), p.Y + NewFixed64(v.Y), p.Z + NewFixed64(v.Z)}
+}
+
+func (p Position) Sub(o Position) V3 {
+	return V3{(p.X-o.X).Float64(), (p.Y-o.Y).Float64(), (p.Z-o.Z).Float64()}
+}
